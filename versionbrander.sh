48198d45560a3568f7a0437fd9d1f87c46d2679c
Alexander Ney <ajax256@me.com>
2014-06-27 11:13:59 +0100

    Update versionbrander.sh
    comment fixes

--- versionbrander.sh
+++ versionbrander.sh
@@ -19,7 +19,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 
-#brands the build with information of the latest git commits, current branch, current user etc
+# Brands the build with information of the latest git commits, current branch, current user etc
 
 PLISTBUDDY='/usr/libexec/PlistBuddy'
 
@@ -28,14 +28,14 @@ GIT_TAG_PREFIX=$2
 
 echo "Branding info.plist: ${INFOPLIST_ABS_PATH}"
 
-# get git commit hash, branch, username and unstashed files
+# Get git commit hash, branch, username and unstashed files
 GIT_HASH=`cd "$PROJECT_DIR";git rev-parse HEAD`
 GIT_BRANCH=`cd "$PROJECT_DIR";git rev-parse --abbrev-ref HEAD`
 GIT_USER=`cd "$PROJECT_DIR";git config user.name`
 BUILD_TIMESTAMP=$(date +%s)
 BUILD_DATE=$(date -u +"%a %b %d %T GMT %Y")
 
-#brand
+# Brand
 $PLISTBUDDY -c "Add :BuildBrand dict " $INFOPLIST_ABS_PATH
 $PLISTBUDDY -c "Add :BuildBrand:BuildTimestamp integer '$BUILD_TIMESTAMP'" $INFOPLIST_ABS_PATH
 $PLISTBUDDY -c "Add :BuildBrand:BuildDate date '$BUILD_DATE'" $INFOPLIST_ABS_PATH
@@ -45,17 +45,17 @@ $PLISTBUDDY -c "Add :BuildBrand:GitUser string '$GIT_USER'" $INFOPLIST_ABS_PATH
 
 if [ -n "$GIT_TAG_PREFIX" ]
 then
-	#get latest testflight* tag from git
+	# Get latest testflight* tag from git
 	GIT_TAGS=(`git tag -l "$GIT_TAG_PREFIX"`)
 	LENGTH=${#GIT_TAGS[@]}
 	LAST_POSITION=$((LENGTH - 1))
 	LATEST_GIT_TAG=${GIT_TAGS[$LAST_POSITION]}
 
-	#get short git log	
+	# Get short git log	
 	echo "adding log messages since tag: $LATEST_GIT_TAG"
 	GIT_LOG=`git log --pretty='%ci %h %an %s %n' $LATEST_GIT_TAG..`
 
-	#iterate trough logs and add to plist
+	# Iterate trough logs and add to plist
 	$PLISTBUDDY -c "Add :BuildBrand:GitLogSinceTag string '$LATEST_GIT_TAG'" $INFOPLIST_ABS_PATH
 	$PLISTBUDDY -c 'Delete :BuildBrand:GitLog' $INFOPLIST_ABS_PATH
 	$PLISTBUDDY -c 'Add :BuildBrand:GitLog array ' $INFOPLIST_ABS_PATH
